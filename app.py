8c57dca05f8aa54d88cc2f25be1c70a899e2edb4
Rajesh7094 <rajubairajesh5@gmail.com>
2025-08-23 21:58:05 +0530

    updated html

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 COMMITTEE_EMAILS = [
     "rajubairajesh5@gmail.com",
-    "gjsabuhurira@gmail.com"
+    "abuhurairagjs@gmail.com"
 ]
 # mkxb cpmb nzio sddl
 app = FastAPI(
